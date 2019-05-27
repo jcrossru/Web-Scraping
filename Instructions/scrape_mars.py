d367f3c0785f6b7ea97a6f8ea9e303895f4c3a00
jcrossru <jcrossru@gmail.com>
2019-05-26 22:17:31 -0400

    update

--- Instructions/scrape_mars.py
+++ Instructions/scrape_mars.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 from splinter import Browser
 import pandas as pd
+from pandas import *
+
 
 def init_browser():
     # @NOTE: Replace the path with your actual path to the chromedriver
@@ -30,15 +32,70 @@ def scrape_info():
     except AttributeError as e:
         print(e)
 
+    # URL of jpl image
+    jpl_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+
+    # Visit URL
+    browser.visit(jpl_url)
+    # Find and click the 'search' button
+    browser.click_link_by_partial_text('FULL IMAGE')
+   # Retrieve page with the requests module
+    response = requests.get(jpl_url)
+    # Create BeautifulSoup object; parse with 'html.parser'
+    soupFigure = BeautifulSoup(response.text, 'html.parser')
+
+    # results are returned as an iterable list
+    results = soupFigure.find('a', class_="fancybox")
+
+    imgLink = results['data-fancybox-href']
+    featured_img_link = 'https://www.jpl.nasa.gov' + imgLink
+
+    # scrape the table
+    res = requests.get("https://space-facts.com/mars/")
+    soup = BeautifulSoup(res.content,'lxml')
+    table = soup.find_all('table')[0] 
+    df = pd.read_html(str(table))
+    df_values = df[0][1]
+    equatorial_diameter = df_values[0] 
+    polar_diameter = df_values[1] 
+    mass = df_values[2] 
+    moons = df_values[3] 
+    orbit_distance = df_values[4] 
+    orbit_period = df_values[5] 
+    surface_temperature = df_values[6] 
+    first_record = df_values[7] 
+    recorded_by = df_values[8] 
+
+    # get mars weather
+    mars_weather_url = 'https://twitter.com/marswxreport?lang=en'
+    # Retrieve page with the requests module
+    weather_resp = requests.get(mars_weather_url)
+    # Create BeautifulSoup object; parse with 'html.parser'
+    weather_soup = BeautifulSoup(weather_resp.text, 'html.parser')
+    # results are returned as an iterable list
+    weather_results = weather_soup.find('p', class_="tweet-text").text   
+
     # Store data in a dictionary
     mars_data = {
         "news_title": news_title,
-        "news_p": news_p
+        "news_p": news_p,
+        "featured_img": featured_img_link,
+        "weather": weather_results,
+        "equatorial_diameter": equatorial_diameter,
+        "polar_diameter":polar_diameter,
+        "mass":mass,
+        "moons":moons,
+        "orbit_distance":orbit_distance,
+        "orbit_period":orbit_period,
+        "surface_temperature":surface_temperature,
+        "first_record":first_record,
+        "recorded_by":recorded_by
     }
  
+
      # Close the browser after scraping
     browser.quit()
-
+# add code for jpg here
     return mars_data
 
 #print(scrape())
